9cb774862841a97dfc456cce55130def99b3c6a2
soleilnoir1989 <58175619+soleilnoir1989@users.noreply.github.com>
2019-12-06 16:05:28 +0100

    Update page_carte.php

--- page_carte.php
+++ page_carte.php
@@ -8,9 +8,13 @@
       echo 'Succès... ';
     }
 
-  $req1= "INSERT INTO `objects` (`id`, `nom`, `x`, `y`, `lock`) VALUES ('1', 'griffe', '600', '400', '0')";
-  $req2= "DELETE FROM `objects` WHERE id=1";
-   mysqli_query($link, $req1);
+  $req1= "INSERT INTO objects (`id`, `nom`, `x`, `y`, `lock`) VALUES ('1', 'griffe', '600', '400', '0')";
+  $req2= "DELETE FROM objects WHERE id=1";
+  // mysqli_query($link, $req1);
+
+  $reqdel="DELETE FROM joueurs WHERE score=0";
+  mysqli_query($link, $reqdel);
 
  ?>
+
 
